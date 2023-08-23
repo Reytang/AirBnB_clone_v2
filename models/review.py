3a425b03cae5a80d1a5dd9b81d021f4011e12e8b
reytang <nubaboub@gmail.com>
2023-08-23 06:02:22 -0700

    airbnb clone

---
+++ models/review.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines Review class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+
+class Review(BaseModel, Base):
+    """Representing a review for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
+    Attributes:
+        __tablename__ (str): name of the MySQL table to store Reviews.
+        text (sqlalchemy String): review description.
+        place_id (sqlalchemy String): review's place id.
+        user_id (sqlalchemy String): review's user id.
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1084), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+
---
+++ models/user.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines  User class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+
+class User(BaseModel, Base):
+    """Representing a user for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
+    Attributes:
+        __tablename__ (str): name of the MySQL table to store users.
+        email: (sqlalchemy String): user's email address.
+        password (sqlalchemy String): user's password.
+        first_name (sqlalchemy String):user's first name.
+        last_name (sqlalchemy String): user's last name.
+        places (sqlalchemy relationship): User-Place relationship.
+        reviews (sqlalchemy relationship): User-Review relationship.
+    """
+    __tablename__ = "users"
+    email = Column(String(125), nullable=False)
+    password = Column(String(125), nullable=False)
+    first_name = Column(String(125))
+    last_name = Column(String(125))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
+
