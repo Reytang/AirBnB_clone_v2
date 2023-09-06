26d5f2e5a40773e69a68b814a3f4cbae5e8719a0
reytang <nubaboub@gmail.com>
2023-09-06 11:51:14 -0700

    setup

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Sets up webservers for deployment: (Run script on both servers)
+# If not done, does the following:
+#     installs Nginx; creates folders /data/, /data/web_static/,
+#     /data/web_static/releases/, /data/web_static/shared,
+#     /data/web_static/releases/test
+#     /data/web_static/releases/test/index.html (with some content)
+# Create symbolic link /data/web_static/current to data/web_static/releases/test
+#     delete and recreate symbolic link each time script's ran
+# Recursively assign ownership of /data/ folder to user and group 'ubuntu'
+# Update the Nginx config to serve content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static)
+#     restart Nginx
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "test of nginx config" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo nginx -t
+sudo service nginx restart
