671ac6391f9019342eed54357d93b168507ceea0
reytang <nubaboub@gmail.com>
2023-09-23 04:11:32 -0700

    update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
     listen on 0.0.0.0, port 5000
-    routes: /:     display "Hello HBNB!"
+    routes: /:         display "Hello HBNB!"
+            /hbnb:     display "HBNB"
+            /c/<text>: display "C" + text (replace underscores with space)
 """
+
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -18,6 +21,12 @@ def hello_hbnb():
 def hbnb():
     """display text"""
     return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """display custom text given"""
+    return "C {}".format(text.replace('_', ' '))
 
 
 if __name__ == "__main__":
